1ce1116397c7fb031bc4ca9bccb71fe455e5a012
Nguyen Ngoc <hoangnn17000@gmail.com>
2026-01-08 08:45:00 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Nguyen',
+        age: 20
+    };
+
     render() {
         return (
 
-            <div> my first Component </div>
+            <div> my name is {this.state.name} </div>
         );
     }
 }
